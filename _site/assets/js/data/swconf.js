d79e0076d2b882474277b387bf83a3450d10a722
Gabriel Won-Held <gabrielwonheld96@gmail.com>
2024-12-31 20:19:45 -0300

    URL corrigida

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1735677698',resources: [
+    cacheName: 'chirpy-1735686399',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
