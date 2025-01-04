feec0b78d660972b586f0964b65ccf35e47d79ff
Gabriel Won-Held <gabrielwonheld96@gmail.com>
2025-01-04 14:34:54 -0300

    Modificando dark-light mode

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,1 +1,27 @@
-const swconf = { cacheName: 'chirpy-1735688846',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ],denyPaths: [ '/example/*' ], purge: false };
+const swconf = {
+  
+    cacheName: 'chirpy-1736007837',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+    ],denyPaths: [
+      
+        
+          '/example/*'
+      
+    ],
+    purge: false
+  
+};
+
